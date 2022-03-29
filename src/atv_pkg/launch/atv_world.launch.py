43ec7be576800711d7d194847b93364c7ba63429
sigridmellemseter <sigrimel@stud.ntnu.no>
2022-03-29 17:03:08 +0200

    buildt package

---
+++ src/atv_pkg/launch/atv_world.launch.py
@@ -0,0 +1,55 @@
+# Copyright 2019 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Demo for spawn_entity.
+Launches Gazebo and spawns a model
+"""
+# A bunch of software packages that are needed to launch ROS2
+import os
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import ThisLaunchFileDir,LaunchConfiguration
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time', default='True')
+    world_file_name = 'forest.world'
+    pkg_dir = get_package_share_directory('atv_pkg')
+
+    os.environ["GAZEBO_MODEL_PATH"] = os.path.join(pkg_dir, 'models')
+
+    world = os.path.join(pkg_dir, 'worlds', world_file_name)
+    launch_file_dir = os.path.join(pkg_dir, 'launch')
+
+    gazebo = ExecuteProcess(
+            cmd=['gazebo', '--verbose', world, '-s', 'libgazebo_ros_init.so', 
+            '-s', 'libgazebo_ros_factory.so'],
+            output='screen')
+
+    #GAZEBO_MODEL_PATH has to be correctly set for Gazebo to be able to find the model
+    #spawn_entity = Node(package='gazebo_ros', node_executable='spawn_entity.py',
+    #                    arguments=['-entity', 'demo', 'x', 'y', 'z'],
+    #                    output='screen')
+    spawn_entity = Node(package='atv_pkg', executable='atv_node',
+                        arguments=['atvkda', 'demo', '0', '0', '1'],
+                        output='screen')
+
+    return LaunchDescription([
+        gazebo,
+        spawn_entity,
+    ])
