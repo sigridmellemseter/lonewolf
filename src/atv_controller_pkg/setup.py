300b3c6ef553203f8454fb4496e95f4eaf615d58
cecilnik <cecilnik@stud.ntnu.no>
2022-03-30 18:53:42 +0200

    added launch file

--- src/atv_controller_pkg/setup.py
+++ src/atv_controller_pkg/setup.py
@@ -1,4 +1,6 @@
+import os
 from setuptools import setup
+from glob import glob
 
 package_name = 'atv_controller_pkg'
 
@@ -10,17 +12,19 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.launch.py'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='cecilnik',
-    maintainer_email='cecilnik@stud.ntnu.no',
+    maintainer_email='focalfossa@todo.todo',
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'atv_controller_node = atv_controller_pkg.atv_controller_node:main'
+          'atv_controller = atv_controller_pkg.atv_controller:main',
+          'atv_estimator = atv_controller_pkg.atv_estimator:main'
         ],
     },
 )
