4b8d02835a709d807f3d7dbd10f3521f141bada0
Cecilie Nikolaisen <71266539+cecilnik@users.noreply.github.com>
2022-04-04 11:51:45 +0200

    Update setup.bash

--- install/setup.bash
+++ install/setup.bash
@@ -21,11 +21,13 @@ _colcon_prefix_chain_bash_source_script() {
 # setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script
 COLCON_CURRENT_PREFIX="/opt/ros/foxy"
 _colcon_prefix_chain_bash_source_script "$COLCON_CURRENT_PREFIX/local_setup.bash"
+
 # setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script
-COLCON_CURRENT_PREFIX="/home/cecilnik/install"
+COLCON_CURRENT_PREFIX="/home/$USER/install"
 _colcon_prefix_chain_bash_source_script "$COLCON_CURRENT_PREFIX/local_setup.bash"
+
 # setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script
-COLCON_CURRENT_PREFIX="/home/cecilnik/dev_ws/install"
+COLCON_CURRENT_PREFIX="/home/$USER/dev_ws/install"
 _colcon_prefix_chain_bash_source_script "$COLCON_CURRENT_PREFIX/local_setup.bash"
 
 # source this prefix
