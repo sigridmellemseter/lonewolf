0cb85e9b307b3cb741baf837de05c0e8d5855dd8
sigridmellemseter <sigrimel@stud.ntnu.no>
2022-04-20 13:17:13 +0200

    Fixed path problem

--- install/setup.bash
+++ install/setup.bash
@@ -23,7 +23,7 @@ COLCON_CURRENT_PREFIX="/opt/ros/foxy"
 _colcon_prefix_chain_bash_source_script "$COLCON_CURRENT_PREFIX/local_setup.bash"
 
 # setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script
-COLCON_CURRENT_PREFIX="/home/$USER/install"
+COLCON_CURRENT_PREFIX="/home/$USER/lonewolf/install"
 _colcon_prefix_chain_bash_source_script "$COLCON_CURRENT_PREFIX/local_setup.bash"
 
 # setting COLCON_CURRENT_PREFIX avoids determining the prefix in the sourced script
